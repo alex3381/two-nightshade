1eccfe1518bca02fac306151b14b359b3c308da3
Alex Fatogun <alex.fatogun@yahoo.com>
2021-07-25 18:51:11 +0100

    Add all my files

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3#i8^e&zyx)87ea&mpp^jcg)cas6^)%2bk)r2@24az2gpa_%f1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['two-nightshade.herokuapp.com' ]
+ALLOWED_HOSTS = ['127.0.0.1:8000','two-nightshade.herokuapp.com' ]
 
 # Application definition
 
